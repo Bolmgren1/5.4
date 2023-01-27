c83e0dc8670cdb0292ea2471a3378d3b24228eaf
Bergqvist1 <bang03001@utb.vaxjo.se>
2023-01-27 10:59:52 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[6];
+
+            namn[0] = "Anton";
+            namn[1] = "Bertil";
+            namn[2] = "Agaton";
+            namn[3] = "Sten";
+            namn[4] = "Peter";
+            namn[5] = "Kevin";
+
+            string första = "Agaton";
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                if (namn[i].CompareTo(första) < 0)
+                {
+                    första = namn[i];
+                }
+            }
+
+            Console.WriteLine("Det första namnet är " + första);
+
+        }
+    }
+}
